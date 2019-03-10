67269a113c9fa818c016e40620839bc0b187ec40
Venkatesh Naidu <venkynaidu1997@gmail.com>
2019-03-11 02:52:42 +0530

    database modifiations done

--- server.js
+++ server.js
@@ -9,18 +9,24 @@ let mkdirp = require('mkdirp');
 let fs = require('fs');
 let fsextra = require('fs-extra');
 
-var config = {
+let config = {
     user: 'postgres',
     database: 'ubuntu',
     host: 'localhost',
     password: 'abcd@123456'
 };
 
-var app = express();
+let app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
 
+let pool = new Pool(config);
+
 app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'ui', 'home.html'));
+});
+
+app.get('/Desktop', function (req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'home.html'));
 });
 
@@ -57,10 +63,27 @@ app.post('/listDirectory', function (req, res) {
 });
 
 app.post('/create', function (req, res) {
-    let dirpath = "./ui/" + req.body.directory_name;
+    let dirpath = "./ui/" + req.body.path;
+    let type = "";
+    if(req.body.name.includes(".")) {
+        type = "file";
+    }
+    else {
+        type = "folder";
+    }
     mkdirp(dirpath, function(err) {
-        if (err) console.log(err);
+        if (err) {
+            console.log(err);
+            res.status(500).send("Something went wrong!");
+        }
         console.log("Successfully created test directory");
+    });
+    pool.query('INSERT INTO "fileSystem" (name, path, status, type) VALUES ($1, $2, $3, $4)', [req.body.name, dirpath, 0, type], function (err, result) {
+        if (err) {
+            console.log("Something went wrong while inserting!");
+        } else {
+            console.log("Successfully inserted in fileSystem table.");
+        }
     });
     res.status(200).send("Successfully created!");
 });
@@ -68,8 +91,20 @@ app.post('/create', function (req, res) {
 app.post('/rename', function (req, res) {
     let currentPath = './ui/' + req.body.currentPath;
     let newPath = './ui/' + req.body.newPath;
+    let currentName = req.body.currentName;
+    let newName = req.body.newName;
     fs.rename(currentPath, newPath, function(err) {
-        if ( err ) console.log('ERROR: ' + err);
+        if ( err ) {
+            console.log('ERROR: ' + err);
+            res.status(500).send("Something went wrong!");
+        }
+    });
+    pool.query('UPDATE "fileSystem" SET name = ($1), path = ($2) WHERE name = ($3) AND path = ($4)', [newName, newPath, currentName, currentPath], function (err, result) {
+        if (err) {
+            console.log("Something went wrong while updating!");
+        } else {
+            console.log("Successfully updated!");
+        }
     });
     res.status(200).send("Successfully changed!");
 });
@@ -78,9 +113,20 @@ app.post('/moveToTrash', function (req, res) {
     let currentPath = './ui/' + req.body.currentPath;
     let newPath = './ui/Desktop/trash/' + req.body.fname;
     fsextra.move(currentPath, newPath, function (err) {
-        if (err) return console.error(err)
-        console.log("success!")
-    })
+        if (err) {
+            console.log(err);
+        }
+        else {
+            console.log("success!");
+        }
+    });
+    pool.query('UPDATE "fileSystem" SET status = ($1), path = ($2) WHERE name = ($3) AND path = ($4)', [1, newPath, req.body.fname, currentPath], function (err, result) {
+        if (err) {
+            console.log("Something went wrong while updating!");
+        } else {
+            console.log("Successfully updated!");
+        }
+    });
     res.status(200).send("Successfully moved to trash!");
 });
 
--- ui/js/common.js
+++ ui/js/common.js
@@ -1,91 +1,96 @@
 let url = "http://localhost:9090/Desktop";
-let arr = {dir_name : 'Desktop'};
+let arr = {dir_name: 'Desktop'};
 let fname = "";
 let elm = "";
 
 $(function () {
-   listDir();
+    listDir();
 });
 
 function listDir() {
-   $.ajax({
-      type: 'POST',
-      dataType: "json",
-      contentType: "application/json",
-      url: "/listDirectory",
-      cache: false,
-      data: JSON.stringify(arr),
-      success: function (response) {
-         $("#main").empty();
-         for(key in response) {
-            let filename = response[key].name;
-            if(response[key].type == "file") {
-               let elem = $(".file.hidden").clone();
-               elem.removeClass("hidden");
-               elem.find("p").html(filename);
-               elem.attr("ondblclick", "updateURL('" + filename.toString() +"')");
-               elem.contextmenu(function (e) {
-                   right_click_on_f(e, filename, elem);
-                   e.preventDefault();
-                   e.stopImmediatePropagation();
-               });
-               $("#main").append(elem);
-               $("#main").append("<br>");
+    $.ajax({
+        type: 'POST',
+        dataType: "json",
+        contentType: "application/json",
+        url: "/listDirectory",
+        cache: false,
+        data: JSON.stringify(arr),
+        success: function (response) {
+            $("#main").empty();
+            for (key in response) {
+                let filename = response[key].name;
+                if (response[key].type == "file") {
+                    let elem = $(".file.hidden").clone();
+                    elem.removeClass("hidden");
+                    elem.find("p").html(filename);
+                    elem.attr("ondblclick", "updateURL('" + filename.toString() + "')");
+                    elem.contextmenu(function (e) {
+                        right_click_on_f(e, filename, elem);
+                        e.preventDefault();
+                        e.stopImmediatePropagation();
+                    });
+                    $("#main").append(elem);
+                    $("#main").append("<br>");
+                } else {
+                    let elem = $(".folder.hidden").clone();
+                    elem.removeClass("hidden");
+                    elem.find("p").html(filename);
+                    elem.attr("ondblclick", "updateURL('" + filename.toString() + "')");
+                    elem.contextmenu(function (e) {
+                        e.preventDefault();
+                        e.stopImmediatePropagation();
+                        right_click_on_f(e, filename, elem);
+                    });
+                    $("#main").append(elem);
+                    $("#main").append("<br>");
+                }
             }
-            else {
-               let elem = $(".folder.hidden").clone();
-               elem.removeClass("hidden");
-               elem.find("p").html(filename);
-               elem.attr("ondblclick", "updateURL('" + filename.toString() +"')");
-               elem.contextmenu(function (e) {
-                   e.preventDefault();
-                   e.stopImmediatePropagation();
-                   right_click_on_f(e, filename, elem);
-               });
-               $("#main").append(elem);
-               $("#main").append("<br>");
-            }
-         }
-         window.history.pushState({}, null, url);
-      },
-      error: function (err) {
-         alert("no!");
-         console.log(err);
-      }
-   });
+            window.history.pushState({}, null, url);
+        },
+        error: function (err) {
+            alert("no!");
+            console.log(err);
+        }
+    });
 }
 
 function updateURL(fname) {
-   if(fname.includes(".")) {
-      alert("This is a File!");
-   }
-   else {
-      url += '/' + fname;
-      arr["dir_name"] += '/' + fname;
-      listDir();
-      window.history.pushState({}, null, url);
-   }
+    if (fname.includes(".")) {
+        alert("This is a File!");
+    } else {
+        if(url == "http://localhost:9090/")
+            url += fname;
+        else
+            url += '/' + fname;
+        arr["dir_name"] += '/' + fname;
+        listDir();
+        window.history.pushState({}, null, url);
+    }
 }
 
-$(window).on('popstate', function(){
-   if(url != window.location.href) {
-      window.history.pushState({}, null, window.location.href);
-      url = window.location.href;
-      arr["dir_name"] = url.split("http://localhost:9090/")[1];
-      listDir();
-   }
+$(window).on('popstate', function () {
+    if (url != window.location.href) {
+        window.history.pushState({}, null, window.location.href);
+        url = window.location.href;
+        arr["dir_name"] = url.split("http://localhost:9090/")[1];
+        listDir();
+    }
 });
 
 function create(param) {
     let obj = {};
-    if(param == "folder") {
+    let name = "";
+    if (param == "folder") {
+        name = $("#foldername").val();
         obj = {
-            "directory_name": arr["dir_name"] + '/' + $("#foldername").val()
+            "path": arr["dir_name"] + '/' + $("#foldername").val(),
+            "name": name
         };
-    }
-    else {
+    } else {
+        name = $("#filename").val();
         obj = {
-            "directory_name": arr["dir_name"] + '/' + $("#filename").val()
+            "path": arr["dir_name"] + '/' + $("#filename").val(),
+            "name": name
         };
     }
     $.ajax({
@@ -137,7 +142,7 @@ function right_click_on_f(e, filename, elem) {
     menu_style.opacity = "1";
     $(document).on("click", function () {
         menu_style.opacity = "0";
-        setTimeout(function() {
+        setTimeout(function () {
             menu_style.visibility = "hidden";
         }, 501);
     });
@@ -148,7 +153,9 @@ function rename(newname) {
     let newPath = arr["dir_name"] + '/' + newname;
     let obj = {
         "currentPath": currentPath,
-        "newPath": newPath
+        "newPath": newPath,
+        "currentName": fname,
+        "newName": newname
     }
     $.ajax({
         type: 'POST',
